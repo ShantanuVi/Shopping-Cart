0ef739d776cf6e49bfe27be44371732f52fa9bed
ShantanuVi <91423728+ShantanuVi@users.noreply.github.com>
2022-11-02 14:56:45 +0530

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,7 +6,7 @@ export default function Card(props) {
   
   return (
     <div className='card'>
-      <img src={props.item.image} alt='product-image' className='product-image' />
+      <img src={props.item.image} alt='product-ima' className='product-image' />
       <span className='product-title'>{props.item.title}</span>
       <span>₹{props.item.price}</span>
       <button className='add-btn' onClick={()=>props.handleAdd(props)}>ADD TO CART</button>
